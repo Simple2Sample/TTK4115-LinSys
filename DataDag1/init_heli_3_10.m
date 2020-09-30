309e7af64507af9fb86183ea1f786e8d32e76747
Simon <simon.saetre@gmail.com>
2020-09-30 10:52:16 +0200

    Day 1
    Day 1

---
+++ DataDag1/init_heli_3_10.m
@@ -0,0 +1,35 @@
+% FOR HELICOPTER NR 3-10
+% This file contains the initialization for the helicopter assignment in
+% the course TTK4115. Run this file before you execute QuaRC_ -> Build 
+% to build the file heli_q8.mdl.
+
+% Oppdatert høsten 2006 av Jostein Bakkeheim
+% Oppdatert høsten 2008 av Arnfinn Aas Eielsen
+% Oppdatert høsten 2009 av Jonathan Ronen
+% Updated fall 2010, Dominik Breu
+% Updated fall 2013, Mark Haring
+% Updated spring 2015, Mark Haring
+
+
+%%%%%%%%%%% Calibration of the encoder and the hardware for the specific
+%%%%%%%%%%% helicopter
+Joystick_gain_x = 1;
+Joystick_gain_y = 1;
+
+
+%%%%%%%%%%% Physical constants
+g = 9.81; % gravitational constant [m/s^2]
+l_c = 0.46; % distance elevation axis to counterweight [m]
+l_h = 0.66; % distance elevation axis to helicopter head [m]
+l_p = 0.175; % distance pitch axis to motor [m]
+m_c = 1.92; % Counterweight mass [kg]
+m_p = 0.72; % Motor mass [kg]
+k_f = 0.713; %Motor Constant [F/V]
+J_p = 2*m_p*(l_p)^2;
+k_1 = l_p * k_f;
+pole_1 = -1;
+pole_2 = -0.3;
+
+K_pp = (pole_1 * pole_2)/(k_1);  %Elevation Proportional Gain
+K_pd = -(pole_1+pole_2)/(k_1);  %Elevation derivational Gain
+
